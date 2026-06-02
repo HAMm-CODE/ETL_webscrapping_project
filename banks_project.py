a8e9e799d040ace9c7a7aaf4d5ba2a9256bc2e0a
Hamm-code <sentsdrimma123@gmail.com>
2026-06-02 15:35:16 +0300

    Enhance logging functionality and update extract method for data retrieval

--- banks_project.py
+++ banks_project.py
@@ -11,13 +11,34 @@ import sqlite3
 def log_progress(message):
     ''' This function logs the mentioned message of a given stage of the
     code execution to a log file. Function returns nothing'''
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
+    now = dt.now()
+    timestamp = now.strftime(timestamp_format)
+    with open('code_log.txt', "a") as f:
+        f.write(f"{timestamp}: {message}")
+
 
 def extract(url, table_attribs):
     ''' This function aims to extract the required
     information from the website and save it to a data frame. The
     function returns the data frame for further processing. '''
-
+    page = requests.get(url).text
+    data = BeautifulSoup(page, 'html.parser')
+    df = pd.DataFrame(columns=table_attribs)
+    tables = data.find_all('tbody')
+    rows = tables[0].find_all('tr')
+    for row in rows:
+        col = row.find_all('td')
+        
+        if len(col)!=0:
+            if col[1].find('a') != -1 and '_' not in col[2]:
+                print()
+                data_dict = {"Bank_Name": col[1].find_all('a')[1].text.strip(),
+                                "MC_USD_Billion": col[2].contents[0].rstrip('\n')}
+                df1 = pd.DataFrame(data_dict, index=[0])
+                df = pd.concat([df,df1], ignore_index=True)
     return df
+
 
 def transform(df, csv_path):
     ''' This function accesses the CSV file for exchange rate
@@ -61,3 +82,5 @@ output_csv_path = "./Largest_banks_data.csv"
 db_name = "Banks.db"
 table_name = "Largest_banks"
 log_file = "code_log.txt"
+
+log_progress('Preliminaries complete. Initiating ETL process')
